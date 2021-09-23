0da8bcbe9517902e9d19a3f8cb3b5ae650319394
foxcatcher24 <tmitul515@gmail.com>
2021-09-23 18:12:12 +0600

    11 -- setting up a HTTP request

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -6,17 +6,32 @@ import Search from './Search';
 
 function Ingredients() {
 
+  const baseURL = 'URL';
   const [ ingredients, setIngredients ] = useState([]);
 
   // Getting data from IngredientForm and adding it current list of ingredients.
   const addIngredientHandler = ingredient => {
     
-    setIngredients( prevIngredients => [ 
-                      ...prevIngredients, { 
-                        id: Math.random().toString(), 
-                        ...ingredient 
-                      } ] 
-                  );
+    // FetchAPI is JS function.
+    fetch( baseURL + '/ingredients.json', {
+      // default method in firebase is GET
+              method: 'POST',
+              body: JSON.stringify( ingredient ),
+              headers: {
+                'Content-Type' : 'application/JSON'
+              }
+    }).then( response => {
+        return response.json();
+    }).then( data => {
+      setIngredients( prevIngredients => [ 
+        ...prevIngredients, { 
+          // name prop returned from firebase, not by us.
+          id: data.name, 
+          ...ingredient 
+        } ] 
+      );
+    });
+
   }
 
   return (
