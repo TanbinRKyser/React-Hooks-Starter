0de52661f38d4727b420aff3e3827c47e8251cf4
foxcatcher24 <tmitul515@gmail.com>
2021-09-26 14:13:43 +0600

    16 -- refs & useRef()

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React,{ useState, useEffect } from 'react';
+import React,{ useState, useEffect, useRef } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
@@ -8,35 +8,48 @@ const Search = React.memo( props => {
   const baseURL = 'URL';
   const [ filter, setFilter ] = useState('');
   const { onLoadIngredients } = props;
+  const inputRef = useRef();
 
   useEffect( () => {
-                // carefully watch the backticks.
-                const queryParams = 
-                        filter.length === 0 
-                        ? '' 
-                        : `?orderBy="title"&equalTo="${filter}"`;
+      
+    setTimeout( () => {
+        if( filter === inputRef.current.value ){
+          // carefully watch the backticks.
+          const queryParams = 
+            filter.length === 0 
+            ? '' 
+            : `?orderBy="title"&equalTo="${filter}"`;
 
-                fetch( baseURL + '/ingredients.json' + queryParams ).then( response => response.json() ).then( resData => {  
-                  const loadedIngredients = [];
-                  
-                  for( let key in resData ){
-                    loadedIngredients.push({
-                      id: key,
-                      title: resData[key].title,
-                      amount: resData[key].amount
-                    });
-                  }
-                  //...
-                  onLoadIngredients(loadedIngredients);
-                });
-  },[ filter, onLoadIngredients ]);
+          fetch( baseURL + '/ingredients.json' + queryParams )
+            .then( response => response.json() )
+              .then( resData => {  
+                const loadedIngredients = [];
+                
+                for( let key in resData ){
+                  loadedIngredients.push({
+                    id: key,
+                    title: resData[key].title,
+                    amount: resData[key].amount
+                  });
+                }
+                //...
+                onLoadIngredients(loadedIngredients);
+          });
+        }
+      }, 500);
+
+  },[ filter, onLoadIngredients, inputRef ]);
 
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" value={ filter } onChange={ event => setFilter( event.target.value ) }  />
+          <input 
+            ref={ inputRef }
+            type="text" 
+            value={ filter } 
+            onChange={ event => setFilter( event.target.value ) }  />
         </div>
       </Card>
     </section>
