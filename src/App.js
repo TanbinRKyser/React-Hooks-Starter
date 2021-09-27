0122ca5bd601d58db8685181208d5d89091f3aa7
foxcatcher24 <tmitul515@gmail.com>
2021-09-27 17:35:00 +0600

    23 -- working with useContext

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import React, { useContext } from 'react';
+import Auth from './components/Auth';
 import Ingredients from './components/Ingredients/Ingredients';
+import { AuthContext } from './context/auth-context';
 
 function App( props ) {
-  return <Ingredients />;;
+  
+  const authContext = useContext( AuthContext );
+
+  let content = <Auth />;
+
+  if( authContext.isAuth ){
+    content = <Ingredients />
+  }
+  
+  return content;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
+import AuthContextProvider from './context/auth-context';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <AuthContextProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </AuthContextProvider>,
   document.getElementById('root')
 );
